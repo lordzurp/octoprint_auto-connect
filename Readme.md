03ecdd39f828380a3941e498938b309389b43ed6
Ti nicO <zurp.online@gmail.com>
2023-04-30 15:50:57 +0200

    remove auto-print function

--- Readme.md
+++ Readme.md
@@ -2,5 +2,6 @@
 
 This plugin simply tries to connect octoprint to your printer if you upload a file.
 
-If the connection is successful it will start the print.
-If it can´t connect for 2 minutes it will abort.
+BUT it will NOT auto-start print :)
+
+forked from Maxinger15/connectandprint
--- auto-connect.py
+++ auto-connect.py
@@ -6,16 +6,16 @@ import flask
 import time
 
 
-class ConnectAndPrintPlugin(octoprint.plugin.EventHandlerPlugin, 
+class AutoConnectPlugin(octoprint.plugin.EventHandlerPlugin, 
                                 octoprint.plugin.RestartNeedingPlugin):
 
     def on_event(self, event, payload):
         if event == octoprint.events.Events.UPLOAD:
             self._logger.info(
                 "Uploaded file detected, connecting and printing...")
-            self._connect_and_print(payload["path"])
+            self._auto_connect(payload["path"])
 
-    def _connect_and_print(self, file_path):
+    def _auto_connect(self, file_path):
         printer = self._printer
 
         if not printer.is_operational():
@@ -28,12 +28,6 @@ class ConnectAndPrintPlugin(octoprint.plugin.EventHandlerPlugin,
         while not printer.is_operational() and time.time() - start_time < timeout:
             time.sleep(1)
 
-        if printer.is_operational():
-            printer.select_file(file_path, False)
-            printer.start_print()
-        else:
-            self._logger.error("Printer connection timed out after 2 minutes.")
-
     def get_update_information(self):
         return dict(
             connectandprint=dict(
@@ -42,23 +36,23 @@ class ConnectAndPrintPlugin(octoprint.plugin.EventHandlerPlugin,
 
                 # Version check: github repository
                 type="github_release",
-                user="Maxinger15",
-                repo="connectandprint",
+                user="lordzurp",
+                repo="octoprint_auto-connect",
                 current=self._plugin_version,
 
                 # Update method: pip
-                pip="https://raw.githubusercontent.com/Maxinger15/connectandprint/{target_version}/connectandprint.py"
+                pip="https://raw.githubusercontent.com/lordzurp/octoprint_auto-connect/{target_version}/auto-connect.py"
             )
         )
 
-__plugin_name__ = "Connect And Print"
+__plugin_name__ = "Auto Connect"
 __plugin_pythoncompat__ = ">=2.7,<4"
 __plugin_version__ = "1.0.0"
-__plugin_description__='Automatically connect to your printer on file upload and start printing'
-__plugin_author__="Max Grallinger"
-__plugin_url__="https://github.com/Maxinger15/connectandprint"
+__plugin_description__='Automatically connect to your printer on file upload'
+__plugin_author__="lordzurp"
+__plugin_url__="https://github.com/lordzurp/octoprint_auto-connect"
 __plugin_license__="AGPLv3"
-__plugin_implementation__ = ConnectAndPrintPlugin()
+__plugin_implementation__ = AutoConnectPlugin()
 __plugin_hooks__ = {
     "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
 }
